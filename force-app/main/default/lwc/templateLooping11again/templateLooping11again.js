9785cb394f5327f701ba76f48f8f47abb11e4f81
AnjliS <140081941+AnjliS@users.noreply.github.com>
2024-06-14 21:07:48 +0530

    commit

---
+++ force-app/main/default/lwc/templateLooping11again/templateLooping11again.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class TemplateLooping11again extends LightningElement {
+    //carList = ["Hundai" , "Toyota" , "Suzuki" , "Mahindra" , "Ford"];
+    countryCapitalList = [
+        { id:1 , coun:"India" , cap:"NewDelhi"},
+        { id:2 , coun:"Japan" , cap:"Tokyo"},
+        { id:3 , coun:"South Korea" , cap:"Seoul"},
+        { id:4 , coun:"Russia" , cap:"Moscow"}
+    ]
+}
